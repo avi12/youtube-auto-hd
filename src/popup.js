5b3cf6233d33a33ff2afddc7a56bdadd6d426f96
avi12 <avi6106@gmail.com>
2020-12-25 22:47:02 +0200

    Only importing necessary values from the storage; using the default values in the components themselves

--- src/popup.js
+++ src/popup.js
@@ -1,33 +1,23 @@
-import Options from "./components/Options.svelte";
+import Options from "./Options.svelte";
 import { getIsFewerQualityValues, getStorage } from "./yt-auto-hd-utilities";
 import { initial } from "./yt-auto-hd-setup";
 
 async function init() {
-  let [
-    qualitiesStored = {},
-    {
-      autoResize: isResizeVideo = initial.autoResize,
-      size: sizeVideo = initial.size,
-      rateDisplay: isDisplayRateOffer = true,
-      isOfferTranslation: isDisplayTranslationOffer = true,
-      isOfferDonation: isDisplaySupportOffer = true,
-    },
-  ] = await Promise.all([getStorage("local", "qualities"), getStorage("sync")]);
+  let [qualitiesStored = {}, { autoResize, size }] = await Promise.all([
+    getStorage("local", "qualities"),
+    getStorage("sync")
+  ]);
 
   if (getIsFewerQualityValues(qualitiesStored, initial.qualities)) {
     qualitiesStored = Object.assign(qualitiesStored, initial.qualities);
   }
-
-  const app = new Options({
+  new Options({
     target: document.body,
     props: {
       qualitiesStored,
-      isResizeVideo,
-      sizeVideo,
-      isDisplayRateOffer,
-      isDisplayTranslationOffer,
-      isDisplaySupportOffer,
-    },
+      isResizeVideo: autoResize,
+      sizeVideo: size
+    }
   });
 }
 
