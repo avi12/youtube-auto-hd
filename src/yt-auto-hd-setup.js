784f965807b9005030a9e487b062dcfaf508a7bd
avi12 <avi6106@gmail.com>
2020-12-25 22:41:37 +0200

    Exporting "qualities"; added the rest of the qualities list

--- src/yt-auto-hd-setup.js
+++ src/yt-auto-hd-setup.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const qualities = [4320, 2160, 1440, 1080, 720];
+export const qualities = [4320, 2160, 1440, 1080, 720, 480, 360, 240, 144];
 const qualityClosest = qualities.find(quality => quality <= screen.height);
 
 export const initial = {
