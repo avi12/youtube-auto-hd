151f85517eafafb890fc4a04af83bea75ec7ab26
avi12 <avi6106@gmail.com>
2021-01-22 13:21:19 +0200

    Added type definitions

--- src/yt-auto-hd-setup.js
+++ src/yt-auto-hd-setup.js
@@ -1,8 +1,25 @@
 "use strict";
 
+/**
+ * @type {number[]}
+ */
 export const qualities = [4320, 2160, 1440, 1080, 720, 480, 360, 240, 144];
+/**
+ * @type {number}
+ */
 const qualityClosest = qualities.find(quality => quality <= screen.height);
 
+/**
+ * @typedef {object} InitialValues
+ * @property {{60: number, 50: number, 30: number}} qualities
+ * @property {"worse"|"best"} qualityFallback
+ * @property {boolean} autoResize
+ * @property {1|0} size
+ */
+
+/**
+ * @type InitialValues
+ */
 export const initial = {
   qualities: {
     60: qualityClosest,
