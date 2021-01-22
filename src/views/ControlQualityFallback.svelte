9f760fba9d5bc9a63dbef2ad92eb8779ad4fa9d0
avi12 <avi6106@gmail.com>
2021-01-22 15:42:48 +0200

    - Changed "worse" to "lower" and "best" to "higher" - Increased subheader bottom gap - Extracted "getI18n()" calls into variables

--- src/views/ControlQualityFallback.svelte
+++ src/views/ControlQualityFallback.svelte
@@ -5,26 +5,39 @@
 
   export let qualityFallback;
 
+  const i18n = {
+    labelSubheader: getI18n("", "Fallback quality"),
+    labelInstruction: getI18n(
+      "",
+      "When the chosen quality is missing, select:"
+    ),
+    labelTooltip: getI18n(
+      "",
+      "E.g. Available qualities are 1080p, 480p, 360p but you choose 720p"
+    ),
+    labelOptionHigher: getI18n("", "Next higher quality"),
+    labelOptionLower: getI18n("", "Next lower quality")
+  };
+
   $: {
     // noinspection JSUnusedAssignment
     chrome.storage.local.set({ qualityFallback });
   }
 </script>
 
-<div class="subheader">{getI18n('', 'Fallback quality')}</div>
-{getI18n('', 'When the chosen quality is missing, select:')}
+<div class="subheader mb-1">{i18n.labelSubheader}</div>
+{i18n.labelInstruction}
 <Tooltip top>
   <Icon path={mdiHelpCircle} />
-  <span
-    slot="tip">{getI18n('', 'E.g. Available qualities are 1080p, 480p, 360p but you choose 720p')}</span>
+  <span slot="tip">{i18n.labelTooltip}</span>
 </Tooltip>
 
 <div class="d-flex flex-column flex-gap mt-1">
-  <Radio bind:group={qualityFallback} value="best">
-    {getI18n('', 'Next best quality')}
+  <Radio bind:group={qualityFallback} value="higher">
+    {i18n.labelOptionHigher}
   </Radio>
-  <Radio bind:group={qualityFallback} value="worse">
-    {getI18n('', 'Next worse quality')}
+  <Radio bind:group={qualityFallback} value="lower">
+    {i18n.labelOptionLower}
   </Radio>
 </div>
 
