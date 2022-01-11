80f135666353ee5603f34c690c7b4d9762fb48ae
avi12 <avi6106@gmail.com>
2022-01-11 18:16:49 +0200

    Content script resize: - Fixed typo - Saving up resources if not resizing

--- src/scripts/ythd-content-script-resize.ts
+++ src/scripts/ythd-content-script-resize.ts
@@ -3,32 +3,31 @@
 import { getStorage } from "../shared-scripts/ythd-utilities";
 import { initial } from "../shared-scripts/ythd-setup";
 
-function getCUrrentSize() {
+function getCurrentSize() {
   const sizeCurrentMatch = document.cookie.match(/wide=([10])/);
   return sizeCurrentMatch ? Number(sizeCurrentMatch[1]) : 0;
 }
 
 export async function resizePlayerIfNeeded(sizeNew?: number) {
+  const elSizeButton = document.querySelector(".ytp-size-button") as HTMLButtonElement;
+  if (!elSizeButton) {
+    return;
+  }
+
   const isAutoResize = (await getStorage("sync", "autoResize")) ?? initial.isResizeVideo;
   if (!isAutoResize) {
     return;
   }
 
   const sizePreferred = sizeNew || ((await getStorage("sync", "size")) ?? initial.size);
-
-  if (getCUrrentSize() === sizePreferred) {
-    return;
-  }
-
-  const elSizeButton = document.querySelector(".ytp-size-button") as HTMLButtonElement;
-  if (!elSizeButton) {
+  if (getCurrentSize() === sizePreferred) {
     return;
   }
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
     await new Promise(resolve => setTimeout(resolve, 100));
-    if (getCUrrentSize() === sizePreferred) {
+    if (getCurrentSize() === sizePreferred) {
       break;
     }
     elSizeButton.click();
