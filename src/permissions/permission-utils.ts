1e736d6dfe8e1ba6e79b41146c9f268c57804f08
avi12 <avi6106@gmail.com>
2022-05-21 09:31:14 +0300

    permission-utils.ts: Specified `permissions`'s type

--- src/permissions/permission-utils.ts
+++ src/permissions/permission-utils.ts
@@ -2,6 +2,6 @@ const additionalDomain = !navigator?.userAgent?.includes("Android")
   ? "https://youtube.googleapis.com/*"
   : "https://m.youtube.com/*";
 
-export const permissions = {
+export const permissions: chrome.permissions.Permissions = {
   origins: ["https://www.youtube.com/*", "https://www.youtube-nocookie.com/*", additionalDomain]
 };
