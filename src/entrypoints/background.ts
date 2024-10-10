23c22b1e8c2eb88e5a30975ed0e4209c55714f81
avi12 <avi6106@gmail.com>
2024-10-10 06:34:09 +0300

    background.ts: Returned to using `storage.setItems`

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,4 +1,4 @@
-import { storage, type StorageArea } from "wxt/storage";
+import { storage, type StorageArea, type StorageItemKey } from "wxt/storage";
 import pathIconOn from "@/public/icon-128.png";
 import pathIconOff from "@/public/icon-off.png";
 
@@ -13,31 +13,30 @@ async function iconActions() {
   storage.watch<boolean>("local:isExtensionEnabled", isEnabled => setIcon(isEnabled!));
 }
 
-chrome.runtime.onInstalled.addListener(async () => {
-  const storageAreas: Array<StorageArea> = ["local", "sync"];
-
-  for (const area of storageAreas) {
-    const Storage = await browser.storage[area].get();
-
-    // @ts-ignore
-    const getValue = value => {
-      try {
-        return JSON.parse(value);
-      } catch {
-        return value;
-      }
-    };
-
-    const storageSets = Object.entries(Storage).map(async ([key, value]) => {
-      console.log(key, value);
-      return storage.setItem(`${area}:${key}`, getValue(value));
-    });
-    await Promise.all(storageSets);
-  }
-
-  await iconActions();
-});
-
 export default defineBackground(() => {
   chrome.runtime.setUninstallURL("");
+  chrome.runtime.onInstalled.addListener(async () => {
+    const storageAreas: Array<StorageArea> = ["local", "sync"];
+
+    for (const area of storageAreas) {
+      const Storage = await browser.storage[area].get();
+
+      // @ts-ignore
+      const getValue = value => {
+        try {
+          return JSON.parse(value);
+        } catch {
+          return value;
+        }
+      };
+
+      const storageSets: Array<{ key: StorageItemKey; value: any }> = Object.entries(Storage).map(([key, value]) => ({
+        key: `${area}:${key}`,
+        value: getValue(value)
+      }));
+      await storage.setItems(storageSets);
+    }
+
+    await iconActions();
+  });
 });
