ce4c7778b4da0bf81d7c0e05df2ccdfd6e544491
avi12 <avi6106@gmail.com>
2024-11-23 08:42:25 +0200

    Simplified background.ts

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -3,36 +3,32 @@ import { getValue } from "@/lib/shared-utils";
 import pathIconOn from "@/public/icon-128.png";
 import pathIconOff from "@/public/icon-off.png";
 
-function setIcon(isEnabled: boolean) {
+function iconActions() {
   const action = chrome.action || chrome.browserAction; // chrome.browserAction for Firefox MV2
-  action.setIcon({
-    path: isEnabled ? pathIconOn : pathIconOff
-  });
-}
-
-async function iconActions() {
-  storage.watch<boolean>("local:isExtensionEnabled", isEnabled => setIcon(isEnabled!));
+  storage.watch<boolean>("local:isExtensionEnabled", isEnabled =>
+    action.setIcon({
+      path: isEnabled! ? pathIconOn : pathIconOff
+    })
+  );
 }
 
 export default defineBackground(() => {
   chrome.runtime.setUninstallURL("");
-  chrome.runtime.onInstalled.addListener(async ({ reason }) => {
-    if (reason !== chrome.runtime.OnInstalledReason.UPDATE) {
-      return;
-    }
-
+  chrome.runtime.onInstalled.addListener(async () => {
     const storageAreas: Array<StorageArea> = ["local", "sync"];
 
     for (const area of storageAreas) {
       const Storage = await browser.storage[area].get();
 
-      const storageSets: Array<{ key: StorageItemKey; value: unknown }> = Object.entries(Storage).map(([key, value]) => ({
-        key: `${area}:${key}`,
-        value: getValue(value)
-      }));
+      const storageSets: Array<{ key: StorageItemKey; value: unknown }> = Object.entries(Storage).map(
+        ([key, value]) => ({
+          key: `${area}:${key}`,
+          value: getValue(value)
+        })
+      );
       await storage.setItems(storageSets);
     }
 
-    await iconActions();
+    iconActions();
   });
 });
