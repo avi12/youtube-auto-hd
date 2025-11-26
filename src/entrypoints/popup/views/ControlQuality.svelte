83c2b8ab957c025c9767cf32505ec443c0c5f8d4
Avi <avi6106@gmail.com>
2025-11-26 19:38:50 +0200

    Added a super resolution toggle

--- src/entrypoints/popup/views/ControlQuality.svelte
+++ src/entrypoints/popup/views/ControlQuality.svelte
@@ -2,7 +2,7 @@
   import { storage } from "#imports";
   import Slider from "../components/Slider.svelte";
   import Switch from "../components/Switch.svelte";
-  import { isEnhancedBitrates, qualitiesStored } from "@/entrypoints/popup/states.svelte";
+  import {isEnhancedBitrates, isUseSuperResolution, qualitiesStored} from "@/entrypoints/popup/states.svelte";
   import type { VideoFPS, VideoQuality } from "@/lib/types";
   import { fpsSupported, qualities } from "@/lib/ythd-setup";
   import { getI18n } from "@/lib/ythd-utils";
@@ -14,7 +14,8 @@
     labelQualityEnd: getI18n("cj_i18n_02148", "FPS videos"),
     preferEnhancedBitrate: getI18n("cj_i18n_07392", "Use enhanced bitrate when it's the highest quality"),
     requiresYouTubePremium: getI18n("cj_i18n_07584", "Requires YouTube Premium"),
-    fpsWarning: getI18n("cj_i18n_07265", "Videos will play at up to 30 FPS for this quality")
+    fpsWarning: getI18n("cj_i18n_07265", "Videos will play at up to 30 FPS for this quality"),
+    labelUseSuperResolution: getI18n("cj_i18n_07966", "Use super resolution when available")
   };
 
   const fpsList = [...fpsSupported].sort((a, b) => a - b);
@@ -41,6 +42,10 @@
 
   $effect(() => {
     storage.setItem("local:isEnhancedBitrates", isEnhancedBitrates.value);
+  });
+
+  $effect(() => {
+    storage.setItem("local:isUseSuperResolution", isUseSuperResolution.value);
   });
 
   function fpsToRange(i: number): string {
@@ -86,7 +91,7 @@
     {:else}
       {#each Object.keys(qualitiesStored.value) as fps, iFps (fps)}
         {#if iFps > 0}
-          <hr class="mt-4" />
+          <hr />
         {/if}
 
         <section class="control-section">
@@ -124,7 +129,13 @@
   {/if}
 </article>
 
-<hr class="mt-4" />
+<hr />
+
+<Switch bind:checked={isUseSuperResolution.value} style="margin-block: 15px">
+  {i18n.labelUseSuperResolution}
+</Switch>
+
+<hr />
 
 <style>
   .control-section {
