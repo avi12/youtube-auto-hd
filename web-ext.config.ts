84b5e5a1774c19d195c13747108c18f978f61add
Avi <avi6106@gmail.com>
2026-01-01 22:07:04 +0200

    web-ext.config.ts: Fixed script

--- web-ext.config.ts
+++ web-ext.config.ts
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
 import { defineWebExtConfig } from "wxt";
 import { execSync } from "child_process";
+import path from "node:path";
 
 const [, , , ...argv] = process.argv;
 const browser = argv.at(-1) || argv.at(-2) || "chrome";
 let executable = "";
 if (browser.match(/chrome|firefox/)) {
-  [, executable] = execSync(`pnpx @puppeteer/browsers install ${browser}@stable --path test-browsers`)
+  [, executable] = execSync(`bunx @puppeteer/browsers install ${browser}@stable --path test-browsers`)
     .toString()
     .trim()
     .split(" ");
 }
-const path = (() => {
+const pathEnv = (() => {
   const osMap: Partial<Record<NodeJS.Platform, string>> = {
     win32: ".env.windows",
     darwin: ".env.mac",
@@ -20,20 +21,21 @@ const path = (() => {
   return osMap[process.platform] || "";
 })();
 
-const env = dotenv.configDotenv({ path, quiet: true }).parsed as {
+const env = dotenv.configDotenv({ path: pathEnv, quiet: true }).parsed as {
   VITE_PATH_EDGE: string;
   VITE_PATH_OPERA: string;
 };
 
 const { VITE_LANG = "en", VITE_BLANK = "" } = process.env;
+
 export default defineWebExtConfig({
   binaries: {
-    ...(executable && { [browser]: executable }),
+    ...(executable && { [browser]: path.resolve(executable) }),
     edge: env.VITE_PATH_EDGE,
-    opera: env.VITE_PATH_OPERA.replace("USERPROFILE", process.env.HOME!)
+    opera: env.VITE_PATH_OPERA.replace("USERPROFILE", process.env.USERPROFILE!)
   },
   disabled: true,
   startUrls: [VITE_BLANK ? "about:blank" : "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
   chromiumArgs: [`--lang=${VITE_LANG}`],
-  ...(browser === "chrome" && VITE_LANG.match(/en|he/) && VITE_BLANK && { disabled: true })
+  ...(browser === "chrome" && VITE_LANG.match(/en|he/) && VITE_BLANK && { disabled: false })
 });
