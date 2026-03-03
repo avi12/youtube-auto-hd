e4c0406b5cc10e815686868067acdca195f00d3b
Avi <avi6106@gmail.com>
2026-03-03 11:07:23 +0200

    Added TypeScript and ESLint strictness

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
 import eslint from "@eslint/js";
 import avi12 from "eslint-config-avi12";
 import svelteEslint from "eslint-plugin-svelte";
+import { globalIgnores } from "eslint/config";
+import perfectionist from "eslint-plugin-perfectionist";
 import globals from "globals";
 import svelteParser from "svelte-eslint-parser";
 import tsEslint from "typescript-eslint";
 
 export default [
+  globalIgnores([".wxt/**", "build/**", "test-browsers/**", "node_modules/**", "eslint.config.js"]),
   eslint.configs.recommended,
   ...tsEslint.configs.recommended,
   ...svelteEslint.configs["flat/recommended"],
@@ -22,12 +25,31 @@ export default [
         ...globals.node,
         chrome: true
       }
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-assertions": ["error", { assertionStyle: "never" }]
     }
   },
   {
-    files: ["**/*.ts"],
+    files: ["**/*.{ts,js}"],
     languageOptions: {
       parser: tsEslint.parser
+    },
+    plugins: {
+      perfectionist
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-assertions": ["error", { assertionStyle: "never" }],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          type: "alphabetical",
+          order: "asc",
+          newlinesBetween: "ignore",
+          sortSideEffects: true,
+          groups: [["side-effect", "builtin", "external", "internal", "parent", "sibling", "index", "unknown"]]
+        }
+      ]
     }
   }
 ];
